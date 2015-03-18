f52b414b873ec083fb4f78d0b859969d5bf4e739
Robert Howerton <wh01683@georgiasouthern.edu>
2015-03-17 20:28:01 -0500

    added try-catches to RealBank

--- src/bank_package/RealBank.java
+++ src/bank_package/RealBank.java
@@ -5,10 +5,7 @@ import acct.AccountFactory;
 import utility.RandomGenerator;
 
 import java.io.Serializable;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 
 public class RealBank implements Serializable, Bank {
@@ -37,7 +34,13 @@ public class RealBank implements Serializable, Bank {
     }
 
     public Hashtable getAccountHashTable() {
-        return accountHashtable;
+        try {
+            return accountHashtable;
+        } catch (NullPointerException n) {
+            System.out.printf("Null pointer caught in RealBank : getAccountHashTable.\nHashTable has not yet" +
+                    " been created. Returning temporary.");
+            return new Hashtable();
+        }
     }
 
     public RealBank getNewRandomBank() {
@@ -49,77 +52,100 @@ public class RealBank implements Serializable, Bank {
 
     public void addRandomCustomers(int numberCustomers) {
 
-        if (customerHashtable == null) {
-            customerHashtable = new Hashtable<Integer, Customer>(numberCustomers * 2);
-        }
-        if (accountHashtable == null) {
-            accountHashtable = new Hashtable<Integer, Account>(numberCustomers * 10);
-        }
+        try {
 
-
-        for (int i = 0; i < numberCustomers; i++) {
-            Customer tempCustomer = new Customer();
-            customerHashtable.put(tempCustomer.getUUID().hashCode(), tempCustomer);
-
-            for (int k = 0; k < r.nextInt(10); k++) { //generates anywhere between 10 and 0 random accounts
-                Account tempAccount = testAccountFactory.getRandomAccount(tempCustomer);
-                if(!(tempAccount == null))
-                    tempCustomer.addAccount(tempAccount);
+            if (customerHashtable == null) {
+                customerHashtable = new Hashtable<Integer, Customer>(numberCustomers * 2);
             }
-            this.addCustomer(tempCustomer);
+            if (accountHashtable == null) {
+                accountHashtable = new Hashtable<Integer, Account>(numberCustomers * 10);
+            }
+
+
+            for (int i = 0; i < numberCustomers; i++) {
+                Customer tempCustomer = new Customer();
+                customerHashtable.put(tempCustomer.getUUID().hashCode(), tempCustomer);
+
+                for (int k = 0; k < r.nextInt(10); k++) { //generates anywhere between 10 and 0 random accounts
+                    Account tempAccount = testAccountFactory.getRandomAccount(tempCustomer);
+                    if (!(tempAccount == null))
+                        tempCustomer.addAccount(tempAccount);
+                }
+                this.addCustomer(tempCustomer);
+            }
+        } catch (NullPointerException n) {
+            System.out.printf("Null pointer caught in RealBank : addRandomCustomers");
         }
     }
 
     public boolean addCustomer(Customer customer) {
 
-        customerHashtable.put(customer.getUUID().hashCode(), customer);
-        this.NUMBER_OF_CUSTOMERS++;
-        Enumeration<Integer> acctKeys = customer.getAccountHashtable().keys();
-        while (acctKeys.hasMoreElements()) {
-            Integer acctKey = acctKeys.nextElement();
-            Account tempAcct = customer.getAccount(acctKey);
-            if (!(tempAcct == null)){
-                accountHashtable.put(acctKey, tempAcct);
-                this.NUMBER_OF_ACCOUNTS++;
-            }
-        }
-        return true;
-    }
-
-    public void addAccount(Account account){
-
-        accountHashtable.put(account.getACCOUNT_NUMBER(), account);
-        customerHashtable.get(account.getOwner().hashCode()).addAccount(account);
-        this.NUMBER_OF_ACCOUNTS++;
-    }
-
-    public boolean removeCustomer(Customer customer){
-
-        if (accountHashtable.contains(customer)) {
-            accountHashtable.remove(customer.getUUID().hashCode());
-            this.NUMBER_OF_CUSTOMERS--;
-
+        try {
+            customerHashtable.put(customer.getUUID().hashCode(), customer);
+            this.NUMBER_OF_CUSTOMERS++;
             Enumeration<Integer> acctKeys = customer.getAccountHashtable().keys();
             while (acctKeys.hasMoreElements()) {
                 Integer acctKey = acctKeys.nextElement();
                 Account tempAcct = customer.getAccount(acctKey);
-                if (!(tempAcct == null)){
-                    accountHashtable.remove(acctKey);
-                    this.NUMBER_OF_ACCOUNTS--;
+                if (!(tempAcct == null)) {
+                    accountHashtable.put(acctKey, tempAcct);
+                    this.NUMBER_OF_ACCOUNTS++;
                 }
-
             }
             return true;
+        } catch (NullPointerException n) {
+            System.out.printf("Null pointer caught in RealBank : addCustomer");
+            return false;
         }
 
-        else
-            return false;
+    }
 
+    public void addAccount(Account account) {
+        try {
+            accountHashtable.put(account.getACCOUNT_NUMBER(), account);
+            customerHashtable.get(account.getOwner().hashCode()).addAccount(account);
+            this.NUMBER_OF_ACCOUNTS++;
+        } catch (NoSuchElementException e) {
+            System.out.printf("No such element caught in RealBank : addAccount.\nOwner not in system.");
+
+        } catch (NullPointerException n) {
+            System.out.printf("Null pointer caught in RealBank : addAccount.");
+        }
+    }
+
+    public boolean removeCustomer(Customer customer) {
+
+        try {
+            if (accountHashtable.contains(customer)) {
+                accountHashtable.remove(customer.getUUID().hashCode());
+                this.NUMBER_OF_CUSTOMERS--;
+
+                Enumeration<Integer> acctKeys = customer.getAccountHashtable().keys();
+                while (acctKeys.hasMoreElements()) {
+                    Integer acctKey = acctKeys.nextElement();
+                    Account tempAcct = customer.getAccount(acctKey);
+                    if (!(tempAcct == null)) {
+                        accountHashtable.remove(acctKey);
+                        this.NUMBER_OF_ACCOUNTS--;
+                    }
+                }
+                return true;
+            }
+        } catch (NullPointerException n) {
+            System.out.printf("Null pointer exception caught in RealBank : removeCustomer");
+            return false;
+        }
+        return false;
     }
 
     @Override
     public boolean hasAccount(Integer accountNumber) {
-        return accountHashtable.containsKey(accountNumber);
+        try {
+            return accountHashtable.containsKey(accountNumber);
+        } catch (NullPointerException n) {
+            System.out.printf("null pointer caught in RealBank : hasAccount");
+            return false;
+        }
     }
 
     @Override
@@ -127,34 +153,43 @@ public class RealBank implements Serializable, Bank {
         try {
             return !customerHashtable.isEmpty() && customerHashtable.containsKey(customerUUID.hashCode());
         } catch (NullPointerException n) {
-            n.printStackTrace();
-            System.out.println("Null pointer exception in real bank at hasCustomer():boolean");
-        }
-        return false;
-    }
-
-    public boolean removeAccount(Integer accountNumber) {
-
-        if (accountHashtable.containsKey(accountNumber)) {
-            Account tempAccountToBeRemoved = accountHashtable.get(accountNumber);
-            customerHashtable.get(accountHashtable.get(accountNumber).getOwner().hashCode()).getAccountHashtable().remove(accountNumber);
-            accountHashtable.remove(accountNumber);
-            this.NUMBER_OF_ACCOUNTS--;
-            return true;
-        } else {
+            System.out.println("Null pointer exception in RealBank : hasCustomer.");
             return false;
         }
     }
 
-    public Customer requestCustomer(UUID customerID){
+    public boolean removeAccount(Integer accountNumber) {
 
-        if(this.customerHashtable.containsKey(customerID.hashCode())){
-            return this.customerHashtable.get(customerID.hashCode());
+        try {
+            if (accountHashtable.containsKey(accountNumber)) {
+                Account tempAccountToBeRemoved = accountHashtable.get(accountNumber);
+                customerHashtable.get(accountHashtable.get(accountNumber).getOwner().hashCode()).getAccountHashtable().remove(accountNumber);
+                accountHashtable.remove(accountNumber);
+                this.NUMBER_OF_ACCOUNTS--;
+                return true;
+            } else {
+                return false;
+            }
+        } catch (NullPointerException n) {
+            System.out.printf("Null pointer caught in RealBank : removeAccount.");
+            return false;
         }
-        else{
+    }
+
+    public Customer requestCustomer(UUID customerID) {
+
+        try {
+            if (this.customerHashtable.containsKey(customerID.hashCode())) {
+                return this.customerHashtable.get(customerID.hashCode());
+            } else {
+                return null;
+            }
+
+        } catch (NullPointerException n) {
+            System.out.printf("Null pointer caught in RealBank : requestCustomer.\nUUID or customerHashTable not " +
+                    "intitialized.");
             return null;
         }
-
     }
 
     @Override
@@ -175,31 +210,50 @@ public class RealBank implements Serializable, Bank {
     }
 
     public String toString() {
-        return this.name + "-" + this.getNUMBER_OF_CUSTOMERS() + "-" + this.getNumberAccounts();
-    }
-
-    public Hashtable getCustomerTable() {
-        return customerHashtable;
-    }
-
-    public void updateAccountTable() {
-
-        Enumeration<Integer> enumKeys = customerHashtable.keys();
-        while (enumKeys.hasMoreElements()) {
-            Integer key = enumKeys.nextElement();
-            Customer temp = customerHashtable.get(key);
-
-            Hashtable<Integer, Account> tempHash = temp.getAccountHashtable();
-            Enumeration<Integer> acctKeys = tempHash.keys();
-
-            while (acctKeys.hasMoreElements()) {
-                Integer acctKey = acctKeys.nextElement();
-                Account tempAcct = tempHash.get(acctKey);
-                if (!(tempAcct == null))
-                    accountHashtable.put(acctKey, tempAcct);
-
-            }
+        try {
+            return this.name + "-" + this.getNUMBER_OF_CUSTOMERS() + "-" + this.getNumberAccounts();
+        } catch (NullPointerException e) {
+            System.out.printf("null pointer caught RealBank : toString.\n instance likely has not been instantiated yet.");
+            return null;
         }
     }
 
+    public Hashtable getCustomerTable() {
+        try {
+            return customerHashtable;
+        } catch (NullPointerException q) {
+            System.out.printf("Null pointer caught in RealBank : getCustomerTable\ncustomer hash table has not been" +
+                    " initialized yet. Returning temp hashTable");
+            return new Hashtable();
+        }
+    }
+
+    public void updateAccountTable() {
+        try {
+            Enumeration<Integer> enumKeys = customerHashtable.keys();
+
+            while (enumKeys.hasMoreElements()) {
+                Integer key = enumKeys.nextElement();
+                Customer temp = customerHashtable.get(key);
+
+                Hashtable<Integer, Account> tempHash = temp.getAccountHashtable();
+                Enumeration<Integer> acctKeys = tempHash.keys();
+
+
+                while (acctKeys.hasMoreElements()) {
+                    Integer acctKey = acctKeys.nextElement();
+                    Account tempAcct = tempHash.get(acctKey);
+                    if (!(tempAcct == null))
+                        accountHashtable.put(acctKey, tempAcct);
+
+                }
+            }
+        } catch (NullPointerException n) {
+            System.out.printf("null pointer caught in RealBank : updateAccountTable");
+        } catch (NoSuchElementException e) {
+            System.out.printf("no such element caught in RealBank : updateAccountTable");
+        }
+
+
+    }
 }
