b7b5262b1363dcd5a27ce117cad6075b53d6d7e6
Robert Howerton <wh01683@georgiasouthern.edu>
2015-03-19 17:40:09 -0500

    java docs for BankProxy finished

--- src/bank_package/BankProxy.java
+++ src/bank_package/BankProxy.java
@@ -12,15 +12,33 @@ public class BankProxy implements Bank {
 
     private static RealBank realBank;
 
+    /**
+     * BankProxy creates a new proxy to a given Real Bank object. used to protect sensitive data in the real bank from
+     * unauthorized access/modification.
+     *
+     * @param newRealBank instance of the Real Bank object to be referenced
+     */
     public BankProxy(RealBank newRealBank) {
         realBank = newRealBank;
     }
 
+    /**
+     * addAccount adds a given account object to the real bank object
+     *
+     * @param newAccount account object to be added
+     * */
     @Override
     public void addAccount(Account newAccount) {
         realBank.addAccount(newAccount);
     }
 
+    /**
+     * requestCustomer requests a single customer from the real bank given a UUID
+     *
+     * @param customerUUID UUID of the requested customer
+     *
+     * @return customer object associated with the given UUID
+     * */
     @Override
     public Customer requestCustomer(UUID customerUUID) {
         if (!(realBank == null)) {
@@ -31,6 +49,13 @@ public class BankProxy implements Bank {
 
     }
 
+    /**
+     * requestCustomerAccounts returns account hash table associated with a single customer
+     *
+     * @param customerUUID UUID of the customer requesting the accounts
+     *
+     * @return returns a Hashtable object with Accounts as values and their respective account numbers as keys
+     * */
     @Override
     public Hashtable requestCustomerAccounts(UUID customerUUID) {
         if (!(realBank == null)) {
@@ -40,6 +65,13 @@ public class BankProxy implements Bank {
         }
     }
 
+    /**
+     * removeAccount removes a specific account from the real bank and from the owner
+     *
+     * @param accountNumber account number of the account object to be deleted
+     *
+     * @return true if successfully deleted, false otherwise
+     * */
     @Override
     public boolean removeAccount(Integer accountNumber) {
 
@@ -51,24 +83,48 @@ public class BankProxy implements Bank {
         }
     }
 
+    /**
+     * hasAccount asks the real bank if an account exists or not
+     *
+     * @param accountNumber account number associated with the account object
+     *
+     * @return returns true if account exists, false otherwise
+     * */
     @Override
     public boolean hasAccount(Integer accountNumber) {
         return realBank.hasAccount(accountNumber);
     }
 
+    /**
+     * hasCustomer asks the bank if a customer exists or not
+     *
+     * @param customerUUID UUID associated with the desired customer
+     *
+     * @return true if customer exists, false otherwise
+     * */
     @Override
     public boolean hasCustomer(UUID customerUUID) {
         return realBank.hasCustomer(customerUUID);
     }
 
+    /**
+     * addCustomer adds a customer and associated accounts to the real bank
+     *
+     * @param customer customer object to be added
+     *
+     * @return returns true if customer was successfully added, false otherwise
+     * */
     @Override
     public boolean addCustomer(Customer customer) {
         realBank.addCustomer(customer);
         return true;
     }
 
-
-    public void saveAllBankDataToFile() {
+/**
+ * saveAllBankDataToFile saves all bank data to file by creating a new temporary instance of the static real bank and
+ *                       writing to a file via object output stream. streams are closed after it is finished.
+ * */
+public void saveAllBankDataToFile() {
 
         RealBank nonStaticRealBank = realBank;
 
