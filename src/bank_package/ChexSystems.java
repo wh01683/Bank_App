5aecb756976a567fcc664d21abd50d482cd04e1f
Robert Howerton <wh01683@georgiasouthern.edu>
2015-03-19 17:29:50 -0500

    java docs for ChexSystems finished

--- src/bank_package/ChexSystems.java
+++ src/bank_package/ChexSystems.java
@@ -3,15 +3,23 @@ import java.io.Serializable;
 import java.util.Random;
 
 /*ToDo: make a more realistic ChexSystems class that calculates ChexSystems score based on customer history (like CreditScore)*/
-class ChexSystems implements Serializable{
+public class ChexSystems implements Serializable {
     private final int CHEX_SYSTEMS_SCORE;
 
+    /**
+     * ChexSystems creates a ChexReport with a randomly generated integer score ranging from 499 to 899
+     */
     public ChexSystems() {
         //ChexSystems score ranges from 100 to 899
         Random r = new Random();
         this.CHEX_SYSTEMS_SCORE = (r.nextInt(499) + 400);
     }
 
+    /**
+     * getScore gets the score associated with this ChexSystems
+     *
+     * @return returns the integer score
+     */
     public int getScore() {
         return this.CHEX_SYSTEMS_SCORE;
     }
