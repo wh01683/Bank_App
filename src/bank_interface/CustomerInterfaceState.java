aa05b13913bdf01eba6e727512139beffa304cad
Robert Howerton <wh01683@georgiasouthern.edu>
2015-03-27 19:44:35 -0400

    factored out several methods unnecessary for gui based operation

--- src/bank_interface/CustomerInterfaceState.java
+++ src/bank_interface/CustomerInterfaceState.java
@@ -5,35 +5,32 @@ public interface CustomerInterfaceState {
 
     /**
      * enterUUID prompts the user for their UUID in the appropriate state
+     * @param email email entered by the user in the gui form, used to retrieve the customer's actual password
      */
-    void enterEmail();
+    void enterEmail(String email);
 
     /**
      * enterPassword prompts the user for their password and checks for correctness in the appropriate state
+     * @param password password entered by the user in the gui form, verified against the customer's actual email
+     *                 address on file
      */
-    void enterPassword();
-
+    void enterPassword(String password);
     /**
-     * startLoginProcess prompts the user to login or create an account in the appropriate state
-     *
-     * @param isRegistered true if the customer would like to register
-     */
-    void startLoginProcess(boolean isRegistered);
-/**
- * logOff saves bank data, changes state to LoggedOffState and brings up first menu. Same in all states.
- * */
-void logOff();
-/**
- * requestInformation prompts the user for commands upon logging in
- * */
-void requestInformation();
+     * logOff saves bank data, changes state to LoggedOffState and brings up first menu. Same in all states.
+     * */
+    void logOff();
+    /**
+     * requestInformation prompts the user for commands upon logging in
+     * */
+    void requestInformation();
 
     /**
      * startTransaction invoked when the user would like to start a transaction in the appropriate states
-     * */
+     */
     void startTransaction();
-/**
- * addAccount creates and adds a user specified account in the appropriate states
- * */
-void addAccount();
+
+    /**
+     * addAccount creates and adds a user specified account in the appropriate states
+     */
+    void addAccount();
 }
--- src/bank_interface/LoggedOffState.java
+++ src/bank_interface/LoggedOffState.java
@@ -33,48 +33,23 @@ public class LoggedOffState implements CustomerInterfaceState {
 
     /**
      * enterUUID not allowed in this state
+     * @param email
      */
     @Override
-    public void enterEmail() {
-        System.out.println("No.");
-        customerInterface.startLoginProcess(false);
+    public void enterEmail(String email) {
+        customerInterface.setCustomerInterfaceState(customerInterface.processUsernameState);
+        customerInterface.enterEmail(email);
     }
 /**
  * enterPassword not allowed in this state
- * */
+ *
+ * @param password*/
 @Override
-    public void enterPassword() {
-        System.out.println("You must enter your UUID first.");
-    customerInterface.startLoginProcess(false);
+public void enterPassword(String password) {
+    customerInterface.setCustomerInterfaceState(customerInterface.processUsernameState);
     }
 
-    /**startLoginProcess: this is the entry point to the program they simply type whether they want to login or register,
-  *           and they are redirected to the appropriate state and the appropriate handling method based on what they choose
-  *
-  *@param isRegistered: default false.
-  *
-  **/
- @Override
- public void startLoginProcess(boolean isRegistered) {
 
-        String loginOrRegister = logInOrRegister.stringGet();
-
-
-        if (loginOrRegister.equalsIgnoreCase("REGISTER")) {
-            //registerNewCustomer(true);
-        } else if (loginOrRegister.equalsIgnoreCase("LOGIN")) {
-            customerInterface.setCustomerInterfaceState(customerInterface.processUsernameState);
-            customerInterface.enterEmail();
-        } else if (loginOrRegister.equalsIgnoreCase("EXIT")) {
-            System.out.printf("Exiting..");
-            System.exit(0);
-        } else {
-            System.out.printf("Request could not be processed.");
-            startLoginProcess(false);
-        }
-
-
-    }
 /**
  * logOff not allowed in this state, user is already logged off
  * */
@@ -82,32 +57,32 @@ public class LoggedOffState implements CustomerInterfaceState {
     public void logOff() {
     customerInterface.saveBankDataToFile();
         System.out.println("You are already logged off.");
-    customerInterface.startLoginProcess(false);
-    }
+
+}
 /**
  * requestInformation not allowed in this state
  * */
 @Override
     public void requestInformation() {
         System.out.println("You must log in first.");
-    customerInterface.startLoginProcess(false);
-    }
+
+}
 /**
  * startTransaction not allowed in this state
  * */
 @Override
     public void startTransaction() {
         System.out.println("You must log in first.");
-    customerInterface.startLoginProcess(false);
-    }
+
+}
 /**
  * addAccount not allowed in this state
  * */
 @Override
     public void addAccount() {
         System.out.println("You must log in first.");
-    customerInterface.startLoginProcess(false);
-    }
+
+}
 
 
     /**
@@ -133,72 +108,7 @@ public class LoggedOffState implements CustomerInterfaceState {
         } else {
             System.out.println("Cannot add customer. Logging off.");
             customerInterface.setCustomerInterfaceState(customerInterface.loggedOffState);
-            customerInterface.startLoginProcess(false);
+
         }
     }
-
-    /**
-     * fillCreditReportInformation This method is used when creating a new customer. This method is only called if the new customer
-     * is older than 17 years old. They are requested to fill in their credit information.
-     *
-     * @param tempAge: age passed to the fill credit report. In reality, it is unnecessary because the user will never
-     *                 see this method if their age is less than 18; the tempAge is printed purely for debugging and verification
-     * @return new CreditReport returns a new credit report for the customer, filled in with their provided information
-     */
-    CreditReport fillCredReportInformation(int tempAge) {
-        double amountOfLatePayments = 0;
-
-        System.out.println("Since you are " + tempAge + " years old, you must provide some credit information.");
-        double credLimit = CREDIT_LIMIT_SCANNER.doubleGet();
-        double accountBalance = CREDIT_BALANCE_SCANNER.doubleGet();
-        int lenCredHistory = CREDIT_HISTORY_SCANNER.intGet();
-        int latePaymentsOnRecord = NUM_LATE_PAYMENTS_SCANNER.intGet();
-        if (latePaymentsOnRecord > 0)
-            amountOfLatePayments = getLatePaymentAmounts(latePaymentsOnRecord);
-        int recentCredInquiries = CREDIT_INQUIRIES_NUMBER.intGet();
-
-        return new CreditReport(tempAge, latePaymentsOnRecord, amountOfLatePayments, recentCredInquiries, credLimit,
-                accountBalance, lenCredHistory);
-    }
-
-    /**
-     * wantsToRegister asks the user if they want to register or not, whilst giving them the option to return, logoff,
-     * exit the system entirely, or register.
-     *
-     * @return true if the customer would like to register.
-     */
-    private boolean wantsToRegister() {
-        final uScanner WANT_REGISTER_SCANNER = new uScanner("Would you like to register? YES, NO, LOGOFF, EXIT", 2, 6);
-
-        String answer = WANT_REGISTER_SCANNER.stringGet();
-
-        if (answer.equalsIgnoreCase("YES")) {
-            return true;
-        } else if (answer.equalsIgnoreCase("NO")) {
-            customerInterface.setCustomerInterfaceState(customerInterface.loggedOffState);
-        } else if (answer.equalsIgnoreCase("LOGOFF")) {
-            customerInterface.logOff();
-        } else if (answer.equalsIgnoreCase("EXIT")) {
-            System.out.println("Exiting.");
-            System.exit(0);
-        } else
-            System.out.println("Incorrect response.");
-        return wantsToRegister();
-
-    }
-
-    /**
-     * getLatePaymentAmounts This method is called by the fillCreditReportInformation method IFF the user had more than
-     * 0 late payments on record
-     *
-     * @param newNumberOfLatePayments: number of latePayments on customer's record
-     * @return latePay.doubleGet() returns a new double value with the total value of late payments on record
-     */
-    private double getLatePaymentAmounts(int newNumberOfLatePayments) {
-        uScanner latePay = new uScanner("You indicated you have " + newNumberOfLatePayments + " late payments on record.\n"
-                + "Please enter the total amount of the late payments.", 0, 2000000000);
-        return latePay.doubleGet();
-    }
-
-
 }
