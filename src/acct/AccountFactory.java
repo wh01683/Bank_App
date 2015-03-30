1f6491f72b885653f1744aa41752c8f23bfae22b
Robert Howerton <wh01683@georgiasouthern.edu>
2015-03-30 14:28:30 -0400

    commented Account Factory. Working on account package documentation & comments

--- src/acct/AccountFactory.java
+++ src/acct/AccountFactory.java
@@ -20,6 +20,16 @@ public class AccountFactory implements Serializable {
 
     /*Account factory for creating and applying to different types of accounts*/
 
+    /**
+     * Method used to create accounts using a factory format. The request from the customer is passed as a string
+     * and it is evaluated and matched to the desired account constructor. The customer applying for the account is also
+     * passed through the params to provide information required to open the account.
+     *
+     * @param accountType String representation of the desired account type.
+     * @param customer    customer applying for the account
+     * @return returns the desired, newly constructed account if the customer qualified for the account, returns null
+     * otherwise
+     */
     public Account getAccount(String accountType, Customer customer) {
         if (accountType == null)
             return null;
