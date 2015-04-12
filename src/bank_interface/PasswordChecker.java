46e2d92b89d5cc3767d5caca2fa66156c5463e8b
Robert Howerton <wh01683@georgiasouthern.edu>
2015-04-12 17:08:52 -0500

    re-enabled password checker and omitted the strength checker for strings

--- src/bank_interface/PasswordChecker.java
+++ src/bank_interface/PasswordChecker.java
@@ -1,16 +1,14 @@
 package bank_interface;
 
-/**
- * Created by robert on 3/18/2015.
- *
- */
 
 
 public class PasswordChecker {
 
 
+    /**
+     * Makes new password checker object. Call method strengthCheck(char[]) to check the strength of a given password
+     */
     PasswordChecker() {
-
     }
 
 
@@ -24,103 +22,51 @@ public class PasswordChecker {
     * */
 
 
-// --Commented out by Inspection START (3/27/15 8:02 PM):
-//    /**checkPasswordWithASCIIValues checks strength of password by examining the decimal integer ASCII values of chars
-//     *                              in the password. after checking for appropriate length, the function uses 4 counters
-//     *                              to keep track of whether or not a lower case letter, uppercase letter, number, and
-//     *                              special char has been seen. counters are incremented when a char has an integer
-//     *                              value within the range specified.
-//     *
-//     * @param userPassword: a char[] array is in the JPasswordField by default, so this method can deal with that
-//     *                      specifically.
-//     * @return true if the password is strong enough and of appropriate length, false otherwise.
-//     */
-//    public boolean checkPasswordWithASCIIValues(final char[] userPassword) {
-//
-//        int numberOfSpecialCharacters = 0;
-//        int numberOfLowerCaseLetters = 0;
-//        int numberOfUpperCaseLetters = 0;
-//        int numberOfNumbers = 0;
-//
-//
-//        if (userPassword.length > 32 | userPassword.length < 8) {
-//            System.out.println("Must be no longer than 16 and no shorter than 8 chars long.");
-//            return false;
-//        } else {
-//            for (char c : userPassword) {
-//                if ((int) c > 126 | (int) c < 33) {
-//                    System.out.println("Outside allowed ASCII ranges.");
-//                    return false;
-//                } else if ((int) c > 47 && (int) c < 58) {
-//                    numberOfNumbers++;
-//                } else if ((int) c > 96 && (int) c < 123) {
-//                    numberOfLowerCaseLetters++;
-//                } else if ((int) c > 64 && (int) c < 91) {
-//                    numberOfUpperCaseLetters++;
-//                } else if (((int) c > 32 && (int) c < 48) | ((int) c > 57 && (int) c < 65) | ((int) c > 90 && (int) c < 97) | ((int) c > 122 && (int) c < 127)) {
-//                    numberOfSpecialCharacters++;
-//                }
-//            }
-//
-//
-//            if (!(numberOfLowerCaseLetters > 0 && numberOfNumbers > 0 && numberOfSpecialCharacters > 0 && numberOfUpperCaseLetters > 0)) {
-//                System.out.println("Did not have 1 of each: lower case, uppercase, special, number.");
-//                return false;
-//            } else {
-//                return true;
-//            }
-//        }
-//    }
-// --Commented out by Inspection STOP (3/27/15 8:02 PM)
+    /**
+     * checks strength of password by examining the decimal integer ASCII values of chars
+     * in the password. after checking for appropriate length, the function uses 4 counters
+     * to keep track of whether or not a lower case letter, uppercase letter, number, and
+     * special char has been seen. counters are incremented when a char has an integer
+     * value within the range specified.
+     *
+     * @param userPassword: a char[] array is in the JPasswordField by default, so this method can deal with that
+     *                      specifically.
+     * @return true if the password is strong enough AND of appropriate length, false otherwise.
+     */
+    public boolean strengthCheck(final char[] userPassword) {
 
-// --Commented out by Inspection START (3/27/15 8:02 PM):
-//    /**checkPasswordWithASCIIValues checks strength of password by examining the decimal integer ASCII values of chars
-//     *                              in the password. after checking for appropriate length, the function uses 4 counters
-//     *                              to keep track of whether or not a lower case letter, uppercase letter, number, and
-//     *                              special char has been seen. counters are incremented when a char has an integer
-//     *                              value within the range specified.
-//     *
-//     * @param stringPassword: when the user enters a password when creating an account, they input as a string.
-//     *
-//     * @return true if the password is strong enough and of appropriate length, false otherwise.
-//     **/
-//
-//    public boolean checkStringPassWithASCIIValues(final String stringPassword) {
-//
-//        int numberOfSpecialCharacters = 0;
-//        int numberOfLowerCaseLetters = 0;
-//        int numberOfUpperCaseLetters = 0;
-//        int numberOfNumbers = 0;
-//
-//        char[] userPassword = stringPassword.toCharArray();
-//
-//
-//        if (userPassword.length > 36 | userPassword.length < 8) {
-//            System.out.println("Must be no longer than 16 and no shorter than 8 chars long.");
-//            return false;
-//        } else {
-//            for (char c : userPassword) {
-//                if ((int) c > 126 | (int) c < 33) {
-//                    System.out.println("Outside allowed ASCII ranges.");
-//                    return false;
-//                } else if ((int) c > 47 && (int) c < 58) {
-//                    numberOfNumbers++;
-//                } else if ((int) c > 96 && (int) c < 123) {
-//                    numberOfLowerCaseLetters++;
-//                } else if ((int) c > 64 && (int) c < 91) {
-//                    numberOfUpperCaseLetters++;
-//                } else if (((int) c > 32 && (int) c < 48) | ((int) c > 57 && (int) c < 65) | ((int) c > 90 && (int) c < 97) | ((int) c > 122 && (int) c < 127)) {
-//                    numberOfSpecialCharacters++;
-//                }
-//            }
-//
-//            if (!(numberOfLowerCaseLetters > 0 && numberOfNumbers > 0 && numberOfSpecialCharacters > 0 && numberOfUpperCaseLetters > 0)) {
-//                System.out.println("Did not have 1 of each: lower case, uppercase, special, number.");
-//                return false;
-//            } else {
-//                return true;
-//            }
-//        }
-//    }
-// --Commented out by Inspection STOP (3/27/15 8:02 PM)
+        int numberOfSpecialCharacters = 0;
+        int numberOfLowerCaseLetters = 0;
+        int numberOfUpperCaseLetters = 0;
+        int numberOfNumbers = 0;
+
+
+        if (userPassword.length > 32 | userPassword.length < 8) {
+            System.out.println("Must be no longer than 16 and no shorter than 8 chars long.");
+            return false;
+        } else {
+            for (char c : userPassword) {
+                if ((int) c > 126 | (int) c < 33) {
+                    System.out.println("Outside allowed ASCII ranges.");
+                    return false;
+                } else if ((int) c > 47 && (int) c < 58) {
+                    numberOfNumbers++;
+                } else if ((int) c > 96 && (int) c < 123) {
+                    numberOfLowerCaseLetters++;
+                } else if ((int) c > 64 && (int) c < 91) {
+                    numberOfUpperCaseLetters++;
+                } else if (((int) c > 32 && (int) c < 48) | ((int) c > 57 && (int) c < 65) | ((int) c > 90 && (int) c < 97) | ((int) c > 122 && (int) c < 127)) {
+                    numberOfSpecialCharacters++;
+                }
+            }
+
+
+            if (!(numberOfLowerCaseLetters > 0 && numberOfNumbers > 0 && numberOfSpecialCharacters > 0 && numberOfUpperCaseLetters > 0)) {
+                System.out.println("Did not have 1 of each: lower case, uppercase, special, number.");
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
 }
